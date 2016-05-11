95bae676652500e6b08d2aee8c3bf62ebfd637cd
maxboyko <maxboykomax@gmail.com>
2016-05-11 00:00:05 +0000

    add basic date validation

--- client/js/app/directives/input.js
+++ client/js/app/directives/input.js
@@ -1,8 +1,27 @@
 angular.module('GoldPulse')
-    .directive('dateInput', function(){
+    .directive('dateInput', function() {
         return {
-          restrict: 'E',
-          templateUrl: '/templates/dateInput.html',
-          controller: 'DateCtrl'
+            restrict: 'E',
+            templateUrl: '/templates/dateInput.html',
+            controller: 'DateCtrl',
+            link: function(scope, element) {
+                scope.$watch('dateStr', function(newVal) {
+                    const invalid = isNaN(Date.parse(newVal));
+                    let input = element.find('.form-control-date');
+                    if (invalid && input.hasClass('valid')) {
+                        input.removeClass('valid').addClass('invalid');
+                    }
+                    else if (invalid) {
+                        input.addClass('invalid');
+                    }
+                    else if (!invalid && input.hasClass('invalid')) {
+                        input.removeClass('invalid').addClass('valid');
+                    }
+                    else if (!invalid) {
+                        input.addClass('valid');
+                    }
+
+                });
+            }
         };
     });

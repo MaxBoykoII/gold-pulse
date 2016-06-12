c85afdc3d95d5105969dce655cc56eeea7451f01
maxboyko <maxboykomax@gmail.com>
2016-06-12 20:51:40 +0000

    Block in new routes

--- client/js/app/app.js
+++ client/js/app/app.js
@@ -1,4 +1,12 @@
 'use strict';
 
-angular.module("GoldPulse", ['ngRoute', 'ngMaterial', 'ngMessages', 'material.svgAssetsCache']);
+angular.module("GoldPulse", ['ngRoute', 'ngMaterial', 'ngMessages', 'material.svgAssetsCache']).config(function ($routeProvider) {
+    $routeProvider.when('/', {
+        templateUrl: './templates/pages/main/index.html'
+    }).when('/sliders', {
+        templateUrl: './templates/pages/sliders/index.html'
+    }).otherwise({
+        redirectTo: '/'
+    });
+});
 //# sourceMappingURL=app.js.map
--- development/js/app/app.js
+++ development/js/app/app.js
@@ -1,1 +1,12 @@
-angular.module("GoldPulse", ['ngRoute','ngMaterial', 'ngMessages', 'material.svgAssetsCache']);
+angular.module("GoldPulse", ['ngRoute', 'ngMaterial', 'ngMessages', 'material.svgAssetsCache'])
+    .config($routeProvider => {
+        $routeProvider.when('/', {
+                templateUrl: './templates/pages/main/index.html'
+            })
+            .when('/sliders', {
+                templateUrl: './templates/pages/sliders/index.html'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    });
